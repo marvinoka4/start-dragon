92f9c22af5cb01ef6eda10f0b2e25642567de04a
marvinoka4 <marvinoka4@gmail.com>
2023-03-31 22:09:15 +0100

    progress-wrap fix

--- assets/scripts/js/vendor/theme.js
+++ assets/scripts/js/vendor/theme.js
@@ -1,3 +1,67 @@
+'use strict';
+var theme = {
+    /**
+     * Theme's components/functions list
+     * Comment out or delete the unnecessary component.
+     * Some components have dependencies (plugins).
+     * Do not forget to remove dependency from src/js/vendor/ and recompile.
+     */
+    init: function () {
+        theme.backgroundImage();
+        theme.pageProgress();
+    },
+
+    /**
+     * Background Image
+     * Adds a background image link via data attribute "data-image-src"
+     */
+    backgroundImage: () => {
+        var bg = document.querySelectorAll(".bg-image");
+        for(var i = 0; i < bg.length; i++) {
+            var url = bg[i].getAttribute('data-image-src');
+            bg[i].style.backgroundImage = "url('" + url + "')";
+        }
+    },
+
+    /**
+     * Page Progress
+     * Shows page progress on the bottom right corner of pages
+     */
+    pageProgress: () => {
+        var progressWrap = document.querySelector('.progress-wrap');
+        if (progressWrap != null) {
+            var progressPath = document.querySelector('.progress-wrap path');
+            var pathLength = progressPath.getTotalLength();
+            var offset = 50;
+            progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+            progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+            progressPath.style.strokeDashoffset = pathLength;
+            progressPath.getBoundingClientRect();
+            progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+            window.addEventListener("scroll", function (event) {
+                var scroll = document.body.scrollTop || document.documentElement.scrollTop;
+                var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+                var progress = pathLength - (scroll * pathLength / height);
+                progressPath.style.strokeDashoffset = progress;
+                var scrollElementPos = document.body.scrollTop || document.documentElement.scrollTop;
+                if (scrollElementPos >= offset) {
+                    progressWrap.classList.add("active-progress")
+                } else {
+                    progressWrap.classList.remove("active-progress")
+                }
+            });
+            progressWrap.addEventListener('click', function (e) {
+                e.preventDefault();
+                window.scroll({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            });
+        }
+    },
+}
+theme.init();
 
 /**-----------------------
  scrollCue.js - ver.2.0.0
@@ -484,3 +548,4 @@ const scrollCue = (function () {
     }
 
 })();
+scrollCue.init();
